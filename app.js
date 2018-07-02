ffaff5c0ad1690e4f809ea1117f7b318eff0d9fb
zhangxing <zxstarxing@gmail.com>
2018-07-02 17:43:57 +0800

    connect to mongodb

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 /*reference Express framework */
 var express = require("express");
 var path = require("path");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://zhangxing:zhangxing1@ds161960.mlab.com:61960/zxstarxingblog");
+let db = mongoose.connection;
+
+db.on("open",()=>{
+    console.log("Conected to Mongodb");
+})
+
+db.on("error",(error)=>{
+    console.log(error);
+})
 
 var app = express();
 
 app.use(express.static("public"));
 
+app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'pug');
 
 
+let Article = require('./models/articles');
+
 app.get('/',(req,res)=>{
-    res.render("index");
+    Article.find({},(error,articles)=>{
+        res.render("index",{articles});
+    })
+    
 })
 
 app.listen(5000);
