0bdcabab6758ef46b05f224f49fc6eb8dc48c6ba
zhangxing <zxstarxing@gmail.com>
2018-07-02 21:52:31 +0800

    add articles

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var path = require("path");
 var mongoose = require("mongoose");
+var bodyParser = require('body-parser')
 
 mongoose.connect("mongodb://zhangxing:zhangxing1@ds161960.mlab.com:61960/zxstarxingblog");
 let db = mongoose.connection;
@@ -16,13 +17,17 @@ db.on("error",(error)=>{
 
 var app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyParser.urlencoded({ extended: false }))
 
 let Article = require('./models/articles');
+
+let articleRouter = require('./routers/articles');
+app.use('/articles',articleRouter);
 
 app.get('/',(req,res)=>{
     Article.find({},(error,articles)=>{
